f9339dce723e61e3d4f7fc2076c2d061496180be
Chris Birch <datapointchris@gmail.com>
2021-12-09 03:07:15 -0700

    Fix binary pandas column names

--- sql_to_csv.py
+++ sql_to_csv.py
@@ -1,11 +1,10 @@
 import argparse
-import pandas as pd
 import pathlib
 from redshift_connection import RedshiftConnection
 
 
 parser = argparse.ArgumentParser(
-    description="Enter '[name]' of a file in sql folder.  Returns '[name].csv' in csv folder"
+    description="Enter a filename in sql folder.  Returns 'name.csv' in csv folder"
 )
 parser.add_argument('-f', '--filename')
 args = parser.parse_args()
@@ -15,11 +14,12 @@ basename = sql_file_path.stem
 
 print('Connecting to Redshift')
 cnxn = RedshiftConnection().connect()
+cursor = cnxn.cursor()
+
+with open(sql_file_path, 'r') as query:
+    sql = query.read()
 
 print(f'Executing: `{filename}`')
-with open(sql_file_path, 'r') as query:
-    df = pd.read_sql_query(query.read(), con=cnxn)
-
-
+df = cursor.execute(sql).fetch_dataframe()
 df.to_csv(f'csv/{basename}.csv')
 print(f'Done:  Saved `{filename}` in csv folder.')
